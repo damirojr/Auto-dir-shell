37128358851dd1441051ff729f866569f82a9cf0
Damiro S <97421651+damirojr@users.noreply.github.com>
2022-04-25 17:37:48 -0300

    Add files via upload

--- autodir.sh
+++ autodir.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+#atualizacoes
+echo "sudo apt update"
+sudo apt update
 
+#Navega ate o diretorio
+echo "cd app/appRss"
 cd app/appRss
+
+#inializa git
+echo "git init"
 git init
+
+
 ls
 code .
